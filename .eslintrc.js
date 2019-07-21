5c2e4ba4c9c05445e88369cbbb05da6ce4c0379b
Joe Hillenbrand <joe@purposeful.com>
2019-07-20 23:11:40 -0700

    baseline node typescript project

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  extends: [
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+  ],
+  parserOptions: {
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+  },
+  rules: {
+    indent: 'off',
+    '@typescript-eslint/indent': ['error', 2],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+  },
+};
