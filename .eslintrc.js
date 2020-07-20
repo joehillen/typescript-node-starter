f1432b87073808cfb6cb6c55850c9190763e1206
Joe Hillenbrand <joehillen@gmail.com>
2020-07-20 13:59:03 -0700

    eslint: don't enforce indent

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,11 @@ module.exports = {
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
   ],
   parserOptions: {
-    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
     sourceType: 'module', // Allows for the use of imports
   },
   rules: {
     indent: 'off',
-    '@typescript-eslint/indent': ['error', 2],
     '@typescript-eslint/explicit-function-return-type': 'off',
   },
 };
